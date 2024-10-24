184c3de05a4df756a41f4b5c99d79c7280a1103d
abardon <a.bardon@betterfly-tourism.com>
2024-10-24 19:47:13 +0200

    Update timeline position by moving it to center and alternate the position (right/left) on the content based on index

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -2,11 +2,14 @@
 import { useScroll, useTransform, motion } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
+import { cn } from "@/utils/cn";
 
 interface TimelineEntry {
   title: React.ReactNode;
   content: React.ReactNode;
 }
+
+/**TODO: Add a mask on the image when the section is not reached */
 
 export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   const ref = useRef<HTMLDivElement>(null);
@@ -15,28 +18,35 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   return (
     <div className="w-full" ref={containerRef}>
       <div ref={ref} className="relative mx-auto pb-20">
-        {data.map((item, index) => (
-          <div key={index} className="flex justify-start pt-10 md:gap-10 md:pt-40">
-            <div className="sticky top-40 z-40 flex max-w-xs flex-col items-center self-start md:w-full md:flex-row lg:max-w-sm">
-              {/**TODO: Add a mask on the image when the section is not reached */}
-              <Image
-                className="absolute left-2 size-12 rounded-full"
-                src={"/img/betterfly-logo.png"}
-                alt="Blog article thumbnail"
-                width={64}
-                height={64}
-                style={{ objectFit: "cover" }}
-              />
-              <h3 className="hidden text-xl font-bold text-neutral-500 md:block md:pl-20 md:text-5xl">{item.title}</h3>
-            </div>
-
-            <div className="relative w-full pl-20 pr-4 md:pl-4">
-              <h3 className="mb-4 block text-left text-2xl font-bold text-neutral-500 md:hidden">{item.title}</h3>
-              {item.content}
-            </div>
-          </div>
-        ))}
         <TimelineProgress containerRef={containerRef} contentRef={ref} />
+        {data.map((item, index) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <div key={index} className={cn("relative flex items-start pt-10 md:pt-40", { "justify-end": !isEven })}>
+              {/** Image and title */}
+              <div
+                className={cn("absolute flex w-1/2 items-center gap-6", {
+                  "right-6": isEven,
+                  "left-6 flex-row-reverse": !isEven
+                })}
+              >
+                <Image
+                  className="rounded-full"
+                  src={"/img/betterfly-logo.png"}
+                  alt="Blog article thumbnail"
+                  width={48}
+                  height={48}
+                  style={{ objectFit: "cover" }}
+                />
+                <h3 className="hidden text-xl font-bold text-neutral-500 md:block md:text-5xl">{item.title}</h3>
+              </div>
+
+              {/** Content */}
+              <div className={cn("relative w-1/2", { "pr-8": isEven, "pl-8": !isEven })}>{item.content}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
@@ -70,14 +80,14 @@ const TimelineProgress = ({ containerRef, contentRef }: TimelineProgressProps) =
       style={{
         height: `${height}px`
       }}
-      className="absolute left-8 top-0 w-[2px] overflow-hidden bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-500 to-transparent to-[99%]"
+      className="absolute left-1/2 top-0 w-0.5 -translate-x-1/2 overflow-hidden bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-500 to-transparent to-[99%]"
     >
       <motion.div
         style={{
           height: heightTransform,
           opacity: opacityTransform
         }}
-        className="absolute top-0 w-[2px] rounded-full bg-gradient-to-t from-purple-500 from-[0%] via-blue-500 via-[10%] to-transparent"
+        className="absolute top-0 w-0.5 rounded-full bg-gradient-to-t from-purple-500 from-[0%] via-blue-500 via-[10%] to-transparent"
       />
     </div>
   );
